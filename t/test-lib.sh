e58a7328279b528bf42e4231c7389c7c5413e65b
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-03-09 10:26:41 +0100

    test-lib: remove test_external
    Remove the test_external function(s) in favor of running the Perl
    tests with a test_expect_success. It's too much of a special case for
    the very tiny win using test_external gives us.
    
    The only advantage of this wrapper added in
    fb32c410087 (t/test-lib.sh: add test_external and
    test_external_without_stderr, 2008-06-19) was that we got the
    Test::More output as-is from the two wrapped scripts.
    
    Thus we would get progress shown with prove(1), nicer output if
    individual tests failed.
    
    These were even the only tests in our code base with a plan in
    advance, so we'd see N/total right away, instead of N/? as with
    everything else.
    
    Now this output looks worse, or:
    
        $ ./t9700-perl-git.sh
        ok 1 - set up test repository
        ok 2 - use t9700/test.pl to test Git.pm
        # passed all 2 test(s)
        1..2
    
    Why make it worse? Because other than test-lib.sh itself it's the only
    TAP emitter in our test suite. I'm about to do some changes to how TAP
    is emitted a parsed, and while it would be possible to make it work
    with these special cases, they're more trouble than they're worth.
    
    I'd need to hoop into TAP::Harness etc. to start the count at 2
    instead of 1, and implement all TAP-munging patches I have twice. See
    the upcoming t/helper/test-tee-tap.c for details.
    
    If an individual Perl test fails here we'll still see what test failed
    with "-v", it just won't be reported as TAP. I think that's a
    worthwhile trade-off in this case.
    
    See my own d998bd4ab67 (test-lib: Make the test_external_* functions
    TAP-aware, 2010-06-24) from test-lib.sh itself for the commit that
    made these TAP-aware in the first place, that code (and more) is being
    deleted here.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -645,8 +645,6 @@ test_fixed=0
 test_broken=0
 test_success=0
 
-test_external_has_tap=0
-
 die () {
 	code=$?
 	# This is responsible for running the atexit commands even when a
@@ -1163,26 +1161,23 @@ test_done () {
 	fi
 	case "$test_failure" in
 	0)
-		if test $test_external_has_tap -eq 0
+		if test $test_remaining -gt 0
 		then
-			if test $test_remaining -gt 0
-			then
-				say_color pass "# passed all $msg"
-			fi
-
-			# Maybe print SKIP message
-			test -z "$skip_all" || skip_all="# SKIP $skip_all"
-			case "$test_count" in
-			0)
-				say "1..$test_count${skip_all:+ $skip_all}"
-				;;
-			*)
-				test -z "$skip_all" ||
-				say_color warn "$skip_all"
-				say "1..$test_count"
-				;;
-			esac
+			say_color pass "# passed all $msg"
 		fi
+
+		# Maybe print SKIP message
+		test -z "$skip_all" || skip_all="# SKIP $skip_all"
+		case "$test_count" in
+		0)
+			say "1..$test_count${skip_all:+ $skip_all}"
+			;;
+		*)
+			test -z "$skip_all" ||
+			say_color warn "$skip_all"
+			say "1..$test_count"
+			;;
+		esac
 
 		if test -z "$debug" && test -n "$remove_trash"
 		then
@@ -1202,11 +1197,8 @@ test_done () {
 		exit 0 ;;
 
 	*)
-		if test $test_external_has_tap -eq 0
-		then
-			say_color berror "# failed $test_failure among $msg"
-			say "1..$test_count"
-		fi
+		say_color berror "# failed $test_failure among $msg"
+		say "1..$test_count"
 
 		exit 1 ;;
 
