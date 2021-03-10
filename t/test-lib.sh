e1e1aac12ab4ac5938e429f275669a8784874bd4
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-03-10 18:36:48 +0100

    test-lib: add bold versions of defined colors
    More of these will be used in follow-up commits, but for now change
    "error" color usage to "berror", same color as before, different name.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -550,11 +550,16 @@ then
 	# substitutions strip trailing newlines).  Given that most
 	# (all?) terminals in common use are related to ECMA-48, this
 	# shouldn't be a problem.
-	say_color_error=$(printf "\033[31;1m") # bold red
+	say_color_error=$(printf "\033[31m") # red
+	say_color_berror=$(printf "\033[31;1m") # bold red
 	say_color_skip=$(printf "\033[34m") # blue
+	say_color_bskip=$(printf "\033[34;1m") # bold blue
 	say_color_warn=$(printf "\033[33m") # brown/yellow
+	say_color_bwarn=$(printf "\033[33;1m") # bold brown/yellow
 	say_color_pass=$(printf "\033[32m") # green
+	say_color_bpass=$(printf "\033[32;1m") # green
 	say_color_info=$(printf "\033[36m") # cyan
+	say_color_binfo=$(printf "\033[36;1m") # bold cyan
 	say_color_reset=$(printf "\033[0m")
 	say_color_="" # no formatting for normal text
 	say_color () {
@@ -575,7 +580,7 @@ TERM=dumb
 export TERM
 
 error () {
-	say_color error "error: $*"
+	say_color berror "error: $*"
 	finalize_junit_xml
 	GIT_EXIT_OK=t
 	exit 1
@@ -702,7 +707,7 @@ test_failure_ () {
 		write_junit_xml_testcase "$1" "      $junit_insert"
 	fi
 	test_failure=$(($test_failure + 1))
-	say_color error "not ok $test_count - $1"
+	say_color berror "not ok $test_count - $1"
 	shift
 	printf '%s\n' "$*" | sed -e 's/^/#	/'
 	test "$immediate" = "" || { finalize_junit_xml; GIT_EXIT_OK=t; exit 1; }
@@ -714,7 +719,7 @@ test_known_broken_ok_ () {
 		write_junit_xml_testcase "$* (breakage fixed)"
 	fi
 	test_fixed=$(($test_fixed+1))
-	say_color error "ok $test_count - $@ # TODO known breakage vanished"
+	say_color berror "ok $test_count - $@ # TODO known breakage vanished"
 }
 
 test_known_broken_failure_ () {
@@ -938,7 +943,7 @@ test_eval_ () {
 
 	if test "$test_eval_ret_" != 0 && want_trace
 	then
-		say_color error >&4 "error: last command exited with \$?=$test_eval_ret_"
+		say_color berror >&4 "error: last command exited with \$?=$test_eval_ret_"
 	fi
 	return $test_eval_ret_
 }
@@ -1142,7 +1147,7 @@ test_done () {
 
 	if test "$test_fixed" != 0
 	then
-		say_color error "# $test_fixed known breakage(s) vanished; please update test(s)"
+		say_color berror "# $test_fixed known breakage(s) vanished; please update test(s)"
 	fi
 	if test "$test_broken" != 0
 	then
@@ -1199,7 +1204,7 @@ test_done () {
 	*)
 		if test $test_external_has_tap -eq 0
 		then
-			say_color error "# failed $test_failure among $msg"
+			say_color berror "# failed $test_failure among $msg"
 			say "1..$test_count"
 		fi
 
