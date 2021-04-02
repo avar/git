fd9a2948e9e2a429075aa51a54d68f7b52e597ab
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-04-02 16:05:50 +0200

    Rush B!

--- object.c
+++ object.c
@@ -201,10 +201,10 @@ void *object_as_type(struct object *obj, enum object_type type)
 		return obj;
 	} else {
 		error(obj->parsed
-			      ? _(object_type_mismatch_msg)
-			      : _(object_maybe_type_mismatch_msg),
-			      oid_to_hex(&obj->oid),
-			      type_name(obj->type), type_name(type));
+		      ? _(object_type_mismatch_msg)
+		      : _(object_maybe_type_mismatch_msg),
+		      oid_to_hex(&obj->oid),
+		      type_name(obj->type), type_name(type));
 		return NULL;
 	}
 }
