5da6af4ca26ef276592ed6468111c71767544bd2
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-04-08 17:59:52 +0200

    use test_rm_loose_oid

--- t/t5310-pack-bitmaps.sh
+++ t/t5310-pack-bitmaps.sh
@@ -266,7 +266,7 @@ test_expect_success 'pack-objects respects --honor-pack-keep (local non-bitmappe
 	pack2=$(git pack-objects pack2 <keepobjects) &&
 	mv pack2-$pack2.* .git/objects/pack/ &&
 	>.git/objects/pack/pack2-$pack2.keep &&
-	rm $(objpath $blob2) &&
+	test_rm_loose_oid "$blob2" &&
 	echo HEAD | git pack-objects --honor-pack-keep --stdout --revs >2a.pack &&
 	git index-pack 2a.pack &&
 	list_packed_objects 2a.idx >2a.objects &&
@@ -356,17 +356,17 @@ test_expect_success 'pack-objects respects --incremental' '
 '
 
 test_expect_success 'pack with missing blob' '
-	rm $(objpath $blob) &&
+	test_rm_loose_oid "$blob" &&
 	git pack-objects --stdout --revs <revs >/dev/null
 '
 
 test_expect_success 'pack with missing tree' '
-	rm $(objpath $tree) &&
+	test_rm_loose_oid "$tree" &&
 	git pack-objects --stdout --revs <revs >/dev/null
 '
 
 test_expect_success 'pack with missing parent' '
-	rm $(objpath $parent) &&
+	test_rm_loose_oid "$parent" &&
 	git pack-objects --stdout --revs <revs >/dev/null
 '
 
