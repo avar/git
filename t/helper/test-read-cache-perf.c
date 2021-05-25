fd608441ae0fb54a346542b5ba2f63820a6c9737
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-05-25 12:17:41 +0200

    test-tools: migrate read-cache-perf to parse_options()
    Change the newly added (but then mostly copy/pasted) read-cache-perf
    to use the parse_options() API. This will make things easier as we add
    new options.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- t/helper/test-read-cache-perf.c
+++ t/helper/test-read-cache-perf.c
@@ -1,15 +1,26 @@
 #include "test-tool.h"
 #include "cache.h"
+#include "parse-options.h"
+
+static const char *read_cache_perf_usage[] = {
+	"test-tool read-cache-perf [<options>...]",
+	NULL
+};
 
 int cmd__read_cache_perf(int argc, const char **argv)
 {
 	struct repository *r = the_repository;
-	int cnt;
+	int cnt = -1;
+	struct option options[] = {
+		OPT_INTEGER(0, "count", &cnt, "number of passes"),
+		OPT_END()
+	};
 
-	if (argc == 2)
-		cnt = strtol(argv[1], NULL, 0);
-	else
-		die("usage: test-tool read-cache-perf [<count>]");
+	argc = parse_options(argc, argv, "test-tools", options,
+			     read_cache_perf_usage, 0);
+	if (argc > 0)
+		usage_msg_opt("Too many arguments.", read_cache_perf_usage,
+			      options);
 
 	setup_git_directory();
 	while (cnt--) {
