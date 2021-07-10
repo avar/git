33a33d4ae2e3b19cc753516ecb936065c7ca11f0
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-07-10 10:19:37 +0200

    Revert this branch, merge is a PITA
    Revert "docs: move chunk-format docs to man section 5"
    
    This reverts commit 7d61fa0616f22d1c4ea2e3e82114770fdbfe8791.
    
    Revert "docs: move bitmap-format docs to man section 5"
    
    This reverts commit ec9304dc343d609fa0ea9d77c19ee0f30b6dc413.
    
    Revert "docs: move signature-format docs to man section 5"
    
    This reverts commit 53057e1ff3b949a729fc534f69a9a85701e0e424.
    
    Revert "docs: move index-format docs to man section 5"
    
    This reverts commit 534f3c88e52f46ae080fe600faa9d9490036948c.
    
    Revert "docs: move commit-graph format docs to man section 5"
    
    This reverts commit 2ed264c7176f4300282988df5f73d7ab18a2279d.
    
    Revert "docs: move http-protocol docs to man section 5"
    
    This reverts commit e8a1437cd2cda69ed7733750239999cd42ec77b5.
    
    Revert "docs: move pack-protocol docs to man section 5"
    
    This reverts commit eb519044655d36cfb038f091301c9354f352de14.
    
    Revert "docs: move pack format docs to man section 5"
    
    This reverts commit 022693306f6bca80339085de923a412c247c5063.
    
    Revert "docs: move protocol-related docs to man section 5"
    
    This reverts commit db3064919c4d209986ed3391d69915b95d61c88f.
    
    Revert "docs: start moving technical "format" docs to man pages"
    
    This reverts commit 6c59b62c4309af805722326fe81008d1dfd33259.

--- builtin/help.c
+++ builtin/help.c
@@ -39,7 +39,6 @@ static const char *html_path;
 static int show_all;
 static int show_guides;
 static int show_user_formats;
-static int show_git_formats;
 enum show_config_type {
 	SHOW_CONFIG_UNSET = 0,
 	SHOW_CONFIG_HUMAN,
@@ -55,7 +54,6 @@ static struct option builtin_help_options[] = {
 	OPT_HIDDEN_BOOL(0, "exclude-guides", &exclude_guides, N_("exclude guides")),
 	OPT_BOOL('g', "guides", &show_guides, N_("print list of useful guides")),
 	OPT_BOOL(0, "user-formats", &show_user_formats, N_("print list of user-facing file formats")),
-	OPT_BOOL(0, "git-formats", &show_git_formats, N_("print list of Git's own file and network formats")),
 	OPT_BOOL('c', "config", &show_config, N_("print all configuration variable names")),
 	OPT_SET_INT_F(0, "config-for-completion-vars", &show_config, "",
 		      SHOW_CONFIG_VARS, PARSE_OPT_HIDDEN),
@@ -76,7 +74,6 @@ static const char * const builtin_help_usage[] = {
 	N_("git help [-g|--guides]"),
 	N_("git help [-c|--config]"),
 	N_("git help [--user-formats]"),
-	N_("git help [--git-formats]"),
 	NULL
 };
 
@@ -589,8 +586,7 @@ int cmd_help(int argc, const char **argv, const char *prefix)
 		usage_with_options(builtin_help_usage, builtin_help_options);
 
 	/* Options that take no further arguments */
-	standalone = show_config || show_guides || show_user_formats ||
-		show_git_formats;
+	standalone = show_config || show_guides || show_user_formats;
 	if (standalone && argc)
 		usage_with_options(builtin_help_usage, builtin_help_options);
 
@@ -614,9 +610,6 @@ int cmd_help(int argc, const char **argv, const char *prefix)
 
 	if (show_user_formats)
 		list_user_formats_help();
-
-	if (show_git_formats)
-		list_git_formats_help();
 
 	if (show_all || show_guides || show_user_formats) {
 		printf("%s\n", _(git_more_info_string));
