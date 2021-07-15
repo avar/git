1f5e65a595cec02c7a4d0da11ee834a3ded75404
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-07-15 11:08:28 +0200

    refs API: remove OID argument to reflog_expire()
    As noted in preceding commits and checked by an assert() the "oid"
    parameter to reflog_expire() is not used anymore, rather it passes
    down its locked OID. Let's remove the this already always-NULL
    parameter we passed in.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- refs/files-backend.c
+++ refs/files-backend.c
@@ -2963,7 +2963,7 @@ static int expire_reflog_ent(struct object_id *ooid, struct object_id *noid,
 }
 
 static int files_reflog_expire(struct ref_store *ref_store,
-			       const char *refname, const struct object_id *oid,
+			       const char *refname,
 			       unsigned int flags,
 			       reflog_expiry_prepare_fn prepare_fn,
 			       reflog_expiry_should_prune_fn should_prune_fn,
@@ -3040,7 +3040,6 @@ static int files_reflog_expire(struct ref_store *ref_store,
 		}
 	}
 
-	assert(!oid);
 	(*prepare_fn)(refname, &lock->old_oid, cb.policy_cb);
 	refs_for_each_reflog_ent(ref_store, refname, expire_reflog_ent, &cb);
 	(*cleanup_fn)(cb.policy_cb);
