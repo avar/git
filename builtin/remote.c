557948adf3fb4fded3c2be9052293f9af8989e43
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-07-15 17:52:04 +0200

    remote.c: don't need this with earlier init?

--- builtin/remote.c
+++ builtin/remote.c
@@ -492,7 +492,6 @@ static int get_head_names(const struct ref *remote_refs, struct ref_states *stat
 	refspec.force = 0;
 	refspec.pattern = 1;
 	refspec.src = refspec.dst = "refs/heads/*";
-	states->heads.strdup_strings = 1;
 	get_fetch_map(remote_refs, &refspec, &fetch_map_tail, 0);
 	matches = guess_remote_head(find_ref_by_name(remote_refs, "HEAD"),
 				    fetch_map, 1);
